0c71e87f9fc0fd948cdfb51a02b226216e89746c
Minho Joh <mhjoh99@gmail.com>
2021-12-22 15:12:02 +0800

    Starting files

---
+++ chatbot_train.py
@@ -0,0 +1,14 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+intents_file = open('intents.json').read()
+intents = json.load(intents_file)
