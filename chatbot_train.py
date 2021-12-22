9b468166b3f0ca88cb0f9d680968e705f1fce1ad
Minho Joh <mhjoh99@gmail.com>
2021-12-22 18:28:26 +0800

    Work on making training data for the chatbot

--- chatbot_train.py
+++ chatbot_train.py
@@ -1,7 +1,9 @@
+from keras.engine import input_layer
+from nltk.util import trigrams
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD
 import random
 
 import nltk
@@ -10,5 +12,77 @@ lemmatizer = WordNetLemmatizer()
 import json
 import pickle
 
-intents_file = open('intents.json').read()
+intents_file = open('intents.json')
 intents = json.load(intents_file)
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['!','?',',','.']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # tokenize each word
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
+        # add documents in the corpus
+        documents.append((word, intent['tag']))
+        # add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+print("documents",documents)
+
+# lemmatize and lower each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+# delete duplicates
+words = sorted(list(set(words)))
+# sort classes and delete duplicates
+classes = sorted(list(set(classes)))
+# documents = combination between patterns and intents
+print(len(documents),"documents")
+# classes = intents
+print(len(classes),"classes",classes)
+# words = all words, vocabulary
+print(len(words), "unique lemmatized words", words)
+
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+
+# create the training data
+training = []
+# create empty array for the output
+output_empty = [0] * len(classes)
+# training set, bag of words for every sentence
+for doc in documents:
+    # initializing bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    word_patterns = doc[0]
+    # lemmatize each word - create base word, in attempt to represent related words
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    # create the bag of words array with 1, if word is found in current pattern
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    training.append([bag, output_row])
+# shuffle the features and make numpy array
+random.shuffle(training)
+training = np.array(training)
+# create training and testing lists. X - patterns, Y - intents
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training data is created")
+
+
+# deep neural network model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# WHAT IS SGD!!!!!!!!!!!!!!!!????????
